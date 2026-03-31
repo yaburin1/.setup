c522bcfeb77127a5484564e65f693553460cab50
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-03-31 18:07:19 +0900

    Update nvim_setup.sh

--- setup_bash/nvim_setup.sh
+++ setup_bash/nvim_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd
 sudo dnf -y update
 sudo dnf -y upgrade
 sudo dnf -y group install development-tools
@@ -7,19 +7,30 @@ sudo dnf -y install gcc-c++
 sudo dnf -y install neovim
 sudo dnf -y install fd-find
 sudo dnf -y install tmux
+sudo dnf -y install openssl-devel
+sudo dnf -y install jq
+sudo dnf -y copr enable wslutilities/wslu fedora-41-x86_64
+sudo dnf -y install wslu
 sudo npm install -g tree-sitter-cli
-
-git config --global core.editor "nvim -w"
-git config --global init.defaultBranch main
-git_home=$(find ~ -type d -name ".dotfiles" | head -n 1)
-
-
-
-mkdir -p "${HOME}/.config"
-
-ln -sfn "${git_home}/.bashrc" "${HOME}/.bashrc"
-ln -sfn "${git_home}/.tmux.conf" "${HOME}/.tmux.conf"
-ln -sfn "${git_home}/.config/nvim" "${HOME}/.config/nvim"
-
-
-source "${git_home}/.bashrc"
+git clone --filter=blob:none --no-checkout https://github.com/yaburin1/.setup.git
+cd .setup
+git sparse-checkout init --no-cone
+git sparse-checkout set nvim/* tmux/.tmux.conf .gitignore atcoder/* bash/.bashrc
+git checkout main
+mkdir ~/.config
+ln -snf ~/.setup/nvim/ ~/.config
+ln -snf ~/.setup/tmux/.tmux.conf ~/
+ln -snf ~/.setup/bash/.bashrc ~/.bashrc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source ~/.bashrc
+cargo install cargo-compete
+mkdir ~/atcoder
+cd ~/atcoder
+echo "2"|cargo compete init atcoder
+ln -snf ~/.setup/atcoder/* ~/atcoder
+sudo tee -a /etc/wsl.conf<<EOF
+[interop]
+enabled = true
+appendWindowsPath = true
+EOF
+exit
