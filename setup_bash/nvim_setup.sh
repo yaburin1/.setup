283bf9f09a7f007a3b84da38292b583e7e9f276a
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-04-02 08:54:37 +0900

    Update nvim_setup.sh

--- setup_bash/nvim_setup.sh
+++ setup_bash/nvim_setup.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 cd
+#更新 ##########################################
 sudo dnf -y update
 sudo dnf -y upgrade
+#neovim ########################################
 sudo dnf -y group install development-tools
 sudo dnf -y install gcc-c++
 sudo dnf -y install neovim
 sudo dnf -y install fd-find
+sudo npm install -g tree-sitter-cli
+###############################################
+#tmux
 sudo dnf -y install tmux
+#github
+sudo dnf -y install gh
+#atcoder #######################################
 sudo dnf -y install openssl-devel
 sudo dnf -y install jq
 sudo dnf -y copr enable wslutilities/wslu fedora-41-x86_64
 sudo dnf -y install wslu
-sudo npm install -g tree-sitter-cli
+#################################################
+
+#setup ##############################################################################
 git clone --filter=blob:none --no-checkout https://github.com/yaburin1/.setup.git
 cd .setup
 git sparse-checkout init --no-cone
+# pull file #####################################################################
 git sparse-checkout set nvim/* tmux/.tmux.conf .gitignore atcoder/* bash/.bashrc
+#################################################################################
 git checkout main
+# ファイル配置(シンボリックリンク) #####################################################
 mkdir ~/.config
 ln -snf ~/.setup/nvim/ ~/.config
 ln -snf ~/.setup/tmux/.tmux.conf ~/
 ln -snf ~/.setup/bash/.bashrc ~/.bashrc
+# rust #################################################################
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source ~/.bashrc
+# atcoder ##############################################################
 cargo install cargo-compete
 rustup toolchain install 1.89.0-x86_64-unknown-linux-gnu
 mkdir ~/atcoder
@@ -34,4 +49,4 @@ sudo tee -a /etc/wsl.conf<<EOF
 enabled = true
 appendWindowsPath = true
 EOF
-exit
+#########################################################################
