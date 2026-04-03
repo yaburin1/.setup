572d2d6902540dc27a0d91c606f8de1722d3f4c8
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-04-04 07:08:38 +0900

    Update nvim_setup.sh

--- setup_bash/nvim_setup.sh
+++ setup_bash/nvim_setup.sh
@@ -7,9 +7,10 @@ sudo dnf -y upgrade
 sudo dnf -y group install development-tools
 sudo dnf -y install gcc-c++
 sudo dnf -y install neovim
-sudo dnf -y install fd-find
-sudo dnf -y install ripgrep
-sudo npm install -g tree-sitter-cli
+sudo dnf -y install fd-find #telescope-nvim
+sudo dnf -y install ripgrep #smart-open
+sudo dnf -y install sqlite sqlite-devel sqlite-tcl #smart-open
+sudo npm install -g tree-sitter-cli #treesitter
 ###############################################
 #tmux
 sudo dnf -y install tmux
