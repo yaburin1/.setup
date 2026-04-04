18ab024d0730e12eb2ed4e6a913b436dba66f195
yaburin1 <ryuseiarekkusu110707@yahoo.jp>
2026-04-04 10:04:05 +0900

    masonのパスを追加

--- nvim/lua/plugins/neovim/lspconfig@.lua
+++ nvim/lua/plugins/neovim/lspconfig@.lua
@@ -1,25 +1,25 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+    },
 
-	{
-		-- [使用]
-		-- nvim/after/lsp/<LSP名>.luaで設定上書き
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" }, --*
-		config=function()
-			vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
-		end,
-	},
-	{
-		-- [必要]
-		-- mason
-		-- [推奨]
-		-- "neovim/nvim-lspconfig" --lsp設定集
-		"mason-org/mason-lspconfig.nvim",
-		event = { "CursorMoved", "CursorHold" }, --*
-		opts = {},
-	},
+    {
+        -- [使用]
+        -- nvim/after/lsp/<LSP名>.luaで設定上書き
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" }, --*
+        config = function()
+            vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
+        end,
+    },
+    {
+        -- [必要]
+        -- mason
+        -- [推奨]
+        -- "neovim/nvim-lspconfig" --lsp設定集
+        "mason-org/mason-lspconfig.nvim",
+        event = { "CursorMoved", "CursorHold" }, --*
+        opts = {},
+    },
 }
