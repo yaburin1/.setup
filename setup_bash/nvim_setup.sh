2baa4fa270e50eec7646a6b85df5bafe641b6a19
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-04-10 08:30:43 +0900

    Update nvim_setup.sh

--- setup_bash/nvim_setup.sh
+++ setup_bash/nvim_setup.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+########################################
+# Fedora-43 for WSL + atcoder + neovim #
+########################################
+
 cd
 #更新 ##########################################
 sudo dnf -y update && sudo dnf -y upgrade
 #neovim ########################################
 sudo dnf -y group install development-tools
+<<<<<<< HEAD
+sudo dnf -y install \
+    gcc-c++ \
+    neovim \
+    fd-find \
+    ripgrep \
+    nodejs-npm \
+    wl-clipboard \
+    sqlite sqlite-devel sqlite-tcl
+sudo npm install -g tree-sitter-cli
+=======
 sudo dnf -y install gcc-c++ \
 fd-find \
 ripgrep \
 sqlite sqlite-devel sqlite-tcl \
 nodejs-npm
 sudo npm install -g tree-sitter-cli #treesitter
+>>>>>>> origin/HEAD
 
 # Neovim 最新パッケージインストール (dnfが対応次第置き換える)############
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
@@ -18,6 +34,14 @@ chmod u+x nvim-linux-x86_64.appimage
 rm nvim-linux-x86_64.appimage
 sudo mv squashfs-root/ /opt/nvim/
 sudo ln -s /opt/nvim/AppRun /usr/bin/nvim
+<<<<<<< HEAD
+
+###############################################
+#その他ツール
+sudo dnf -y install \
+    tmux \
+    gh
+=======
 ######################################################################
 #tmux
 sudo dnf -y install tmux
@@ -25,8 +49,10 @@ sudo dnf -y install tmux
 sudo dnf -y install gh
 #wslクリップボード
 sudo dnf -y install wl-clipboard 
+>>>>>>> origin/HEAD
 
 #Rust Iced GUI libX11 ###################################
+#Xserver
 #sudo dnf -y install libX11-devel
 #sudo dnf -y install libXext-devel
 #sudo dnf -y install libXcursor-devel
@@ -42,15 +68,17 @@ sudo dnf -y install wl-clipboard
 #sudo dnf -y install fcitx5
 #sudo dnf -y install fcitx5-mozc
 #sudo dnf -y install dbus-x11
-#そのままでは使えないので以下のurl手順を実行 
+#そのままでは使えないので以下のurl手順を実行
 #https://github.com/microsoft/WSL/issues/10205#issuecomment-2198582165
 #全角半角キー連打を解消
 #sudo dnf -y install xset
 #sudo xset -r 49
 # fcitx5-configtoolでmzucに設定
+
 #atcoder ##############################################
-sudo dnf -y install openssl-devel
-sudo dnf -y install jq
+sudo dnf -y install \
+    openssl-devel \
+    jq
 sudo dnf -y copr enable wslutilities/wslu fedora-41-x86_64
 sudo dnf -y install wslu
 #################################################
@@ -60,15 +88,17 @@ git clone --filter=blob:none --no-checkout https://github.com/yaburin1/.setup.gi
 cd .setup
 git sparse-checkout init --no-cone
 # pull file #####################################################################
-git sparse-checkout set nvim/* tmux/.tmux.conf .gitignore atcoder/* bash/.bashrc /git/.gitconfig
+git sparse-checkout set nvim/* tmux/.tmux.conf .gitignore atcoder/* bash/* /git/.gitconfig /setup_bash/nvim_setup.sh
 #################################################################################
 git checkout main
+sudo cp -r /mnt/e/.setup/.ssh/ ~/
 # ファイル配置(シンボリックリンク) #####################################################
 mkdir ~/.config
 ln -snf ~/.setup/git/.gitconfig ~/.gitconfig
 ln -snf ~/.setup/nvim/ ~/.config
 ln -snf ~/.setup/tmux/.tmux.conf ~/
 ln -snf ~/.setup/bash/.bashrc ~/.bashrc
+ln -snf ~/.setup/bash/.dircolors ~/.dircolors
 # rust #################################################################
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source ~/.bashrc
@@ -77,11 +107,12 @@ cargo install cargo-compete
 rustup toolchain install 1.89.0-x86_64-unknown-linux-gnu
 mkdir ~/atcoder
 cd ~/atcoder
-echo "2"|cargo compete init atcoder
+echo "2" | cargo compete init atcoder
 ln -snf ~/.setup/atcoder/* ~/atcoder
-sudo tee -a /etc/wsl.conf<<EOF
+sudo tee -a /etc/wsl.conf <<EOF
 [interop]
 enabled = true
 appendWindowsPath = true
 EOF
 #########################################################################
+
