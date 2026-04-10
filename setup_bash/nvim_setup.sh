54da08d240817990369969e1769c204533df94ab
yaburin <ryuseiarekkusu110707@yahoo.co.jp>
2026-04-10 16:18:05 +0900

    Update nvim_setup.sh

--- setup_bash/nvim_setup.sh
+++ setup_bash/nvim_setup.sh
@@ -34,8 +34,7 @@ sudo dnf -y install \
 #Rust Iced ##################################
 sudo dnf install -y mesa-vulkan-drivers vulkan-loader
 #IME
-sudo dnf -y install fcitx5-mozc
-
+sudo dnf -y install fcitx5 fcitx5-mozc fcitx5-configtool 
 #そのままでは使えないので以下のurl手順を実行
 #https://github.com/microsoft/WSL/issues/10205#issuecomment-2198582165
 #全角半角キー連打を解消
